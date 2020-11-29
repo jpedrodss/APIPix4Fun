8460556472008cb3766009261744badf8a682241
GabrielMeneze <65770195+GabrielMeneze@users.noreply.github.com>
2020-11-29 19:27:39 -0300

    scaffold

---
+++ Domains/Fotos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIPix4Fun.Domains
+{
+    public partial class Fotos
+    {
+        public Fotos()
+        {
+            Pack = new HashSet<Pack>();
+        }
+
+        public int IdFotos { get; set; }
+        public string FraseFoto { get; set; }
+
+        public virtual ICollection<Pack> Pack { get; set; }
+    }
+}
---
+++ Domains/Pack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APIPix4Fun.Domains
+{
+    public partial class Pack
+    {
+        public Pack()
+        {
+            Pedido = new HashSet<Pedido>();
+        }
+
+        public int IdPack { get; set; }
+        public int? IdFotos { get; set; }
+        public string TipoPack { get; set; }
+        public float? Preco { get; set; }
+
+        public virtual Fotos IdFotosNavigation { get; set; }
+        public virtual ICollection<Pedido> Pedido { get; set; }
+    }
+}
