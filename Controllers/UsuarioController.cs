0e6bbca568a010504777f3cadae023e67e6548fd
GabrielMeneze <65770195+GabrielMeneze@users.noreply.github.com>
2020-11-30 12:41:46 -0300

    Controllers prontos

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIPix4Fun.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIPix4Fun.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuarioController()
+        {
+            UsuarioRepository = new _usuarioRepository();
+        }
+
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+        }
+
+        // GET api/<UsuarioController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsuarioController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsuarioController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsuarioController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
