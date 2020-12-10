d7cbc5d509f7abde421c8c12bcbeca468f2a54b2
João Pedro <jpedrodss@gmail.com>
2020-12-10 01:36:59 -0300

    Controller de upload feito

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,38 @@
+﻿using APIPix4Fun.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPix4Fun.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromForm] IFormFile arquivo)
+        {
+            try
+            {
+                if (arquivo != null)
+                {
+                    var urlImagem = Upload.Local(arquivo);
+
+                    return Ok(new { url = urlImagem });
+                }
+
+                return BadRequest(new
+                {
+                    messagem = "Arquivo não informado"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
